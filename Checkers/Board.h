beccdd3fe74d03b6181b42ea7f5c22af560e0743
Argon12345 <eryk.galica@gmail.com>
2015-07-15 18:51:45 +0200

    commit

---
+++ Checkers/Board.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+enum FIELD{
+	WHITE_K = -2,
+	WHITE = -1,
+	EMPTY = 0,
+	BLACK = 1,
+	BLACK_K = 2
+};
+
+enum PLAYER{
+	P_WHITE = -1,
+	P_BLACK = 1
+};
+
+class Board {
+public:
+	Board();
+	~Board();
+	bool checkMoveTo(int, int, int, int);
+	bool checkBeatings(PLAYER);
+	bool checkBeating(int,int);
+	bool moveTo(int, int, int, int);
+	void promotion(int, int);
+	void Display();
+
+protected:
+	FIELD board[8][8];
+};
+
---
+++ Checkers/graphic.h
@@ -0,0 +1,16 @@
+#include "Board.h"
+#include "SFML\Graphics.hpp"
+
+
+using namespace sf;
+
+class Graphic : public Board
+{
+public:
+	Graphic();
+	~Graphic();
+	void run();
+private:
+
+};
+
